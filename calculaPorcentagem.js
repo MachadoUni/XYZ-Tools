f7726b266bb4549d1c8a05729a01d488476f12e9
MachadoUni <gabriel.machado@edu.unifil.br>
2025-08-15 20:35:50 -0300

    Calculadora de porcentagem e gerador de CPF prontos, além da adição das logos

--- calculaPorcentagem.js
+++ calculaPorcentagem.js
@@ -1,10 +1,69 @@
-function calculaPorcentagem(porcentagem, valor){
+function limparInput(){
+    var inputPorcentagem = document.getElementById("porcentagem");
+    var inputValor = document.getElementById("valor");
+
+    inputPorcentagem.value = '';
+    inputValor.value = '';
+
+    esconderErro();
+    esconderResultado();
+}
+
+function calculaPorcentagem(){
+    esconderErro();
+    esconderResultado();
+
+    var porcentagem = document.getElementById("porcentagem").value;
+    var valor = document.getElementById("valor").value;
     var resultado;
 
-    if (isNaN(porcentagem) || isNaN(valor)){
-        // erro parça
+    porcentagem = String(porcentagem).replace(",", ".");
+    valor = String(valor).replace(",", ".");
+
+    porcentagem = Number(porcentagem);
+    valor = Number(valor);
+    console.log(valor);
+
+    // var porcentagem = Number(porcentagemStr);
+    // var valor = Number(valorStr);
+
+    if (isNaN(porcentagem) || isNaN(valor) || porcentagem == '' || valor == ''){
+        mostrarErro();
+        return;
     }
 
     resultado = (porcentagem / 100) * valor;
+    mostrarResultado(resultado);
+}
+
+function esconderErro(){
+    const erroDiv = document.getElementById("mensagem-erro");
+    if(!erroDiv.classList.contains('hidden')){
+        erroDiv.classList.add('hidden');
+    }
+}
+
+function mostrarErro(){
+    const erroDiv = document.getElementById("mensagem-erro");
+    erroDiv.classList.remove('hidden');
+
+    var mensagem = '<i class="fas fa-exclamation-circle mr-2"></i>Valores inválidos!';
+    erroDiv.innerHTML = `<p class="font-semibold">${mensagem}</p>`;
+}
+
+function esconderResultado(){
+    const resultadoDiv = document.getElementById("resultado-container");
+    if(!resultadoDiv.classList.contains('hidden')){
+        resultadoDiv.classList.add('hidden');
+    }
 
 }
+
+function mostrarResultado(resultado){
+    const resultadoDiv = document.getElementById("resultado-container");
+    resultadoDiv.classList.remove('hidden');
+
+    document.getElementById("resultado").innerHTML = resultado;
+
+}
+
---
+++ geraCPF.js
@@ -0,0 +1,45 @@
+function copiarOutput() {
+  var output = document.getElementById("cpf-output").value;
+
+  if (output !== '') {
+    try {
+      navigator.clipboard.writeText(output);
+    } catch (err) {
+      console.error('Falha ao copiar texto: ', err);
+    }
+  }
+
+}
+
+function gerarCpf() {
+
+  cpfOutput = document.getElementById("cpf-output");
+
+  const num1 = aleatorio();
+  const num2 = aleatorio();
+  const num3 = aleatorio();
+  const dig1 = dig(num1, num2, num3);
+  const dig2 = dig(num1, num2, num3, dig1);
+
+  cpfOutput.value = `${num1}.${num2}.${num3}-${dig1}${dig2}`;
+}
+
+function dig(n1, n2, n3, n4) {
+  const nums = n1.split("").concat(n2.split(""), n3.split(""));
+  if (n4 !== undefined) {
+    nums[9] = n4;
+  }
+
+  let x = 0;
+  for (let i = (n4 !== undefined ? 11 : 10), j = 0; i >= 2; i--, j++) {
+    x += parseInt(nums[j]) * i;
+  }
+
+  const y = x % 11;
+  return y < 2 ? 0 : 11 - y;
+}
+
+function aleatorio() {
+  const aleat = Math.floor(Math.random() * 999);
+  return ("" + aleat).padStart(3, '0');
+}
